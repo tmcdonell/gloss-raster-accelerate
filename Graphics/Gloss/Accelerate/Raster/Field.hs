10c48864abe3e3a1ef91279c7d89ea0fde95e4b4
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2013-08-17 23:43:51 +1000

    change the type of Render
     * instead of a datatype enumerating the backends we know about, it is now a function with the type of `run1`.
     * still includes a default backend, but is otherwise mostly decoupled

--- Graphics/Gloss/Accelerate/Raster/Field.hs
+++ Graphics/Gloss/Accelerate/Raster/Field.hs
@@ -8,7 +8,7 @@ module Graphics.Gloss.Accelerate.Raster.Field (
   module Graphics.Gloss.Accelerate.Data.Color,
 
   -- * Display functions
-  Render(..), Display(..),
+  Render, Display(..),
   animateField, animateFieldWith,
   playField, playFieldWith,
 
@@ -51,18 +51,23 @@ animateField
             --   It is passed the time in seconds since the program started, and
             --   a point between (-1,1) and (+1,1).
     -> IO ()
-animateField = animateFieldWith maxBound
+animateField = animateFieldWith defaultRender
 
 
--- | Animate a continuous 2D function
+-- | Animate a continuous 2D function, specifying the backend used to render
+--   the field.
 --
 animateFieldWith
-    :: Render
-    -> Display
-    -> (Int, Int)
+    :: Render           -- ^ Method to render the field
+    -> Display          -- ^ Display mode
+    -> (Int, Int)       -- ^ Number of pixels to draw per point
     -> (Exp Float -> Exp Point -> Exp Color)
+            -- ^ A function to compute the colour at a particular point.
+            --
+            --   It is passed the time in seconds since the program started, and
+            --   a point between (-1,1) and (+1,1).
     -> IO ()
-animateFieldWith backend display (zoomX, zoomY) makePixel
+animateFieldWith render display (zoomX, zoomY) makePixel
   | zoomX < 1 || zoomY < 1
   = error "Graphics.Gloss.Raster.Field: invalid pixel scalar factor"
 
@@ -71,13 +76,13 @@ animateFieldWith backend display (zoomX, zoomY) makePixel
         -- Size of the raw image to render
         (winSizeX, winSizeY)    = sizeOfDisplay display
 
-        picture time            = makePicture backend winSizeX winSizeY zoomX zoomY (\t -> makePixel (the t))
+        picture time            = makePicture render winSizeX winSizeY zoomX zoomY (\t -> makePixel (the t))
                                 $ fromList Z [time]
     in
     animateFixedIO display G.black (return . picture)
 
 
--- | Play a game with a continuous 2D function.
+-- | Play a game with a continuous 2D function using the default backend.
 --
 playField
     :: Arrays world
@@ -94,22 +99,29 @@ playField
             -- ^ Step the world one iteration.
             --   It is passed the time in seconds since the program started.
     -> IO ()
-playField = playFieldWith maxBound
+playField = playFieldWith defaultRender
 
 
+-- | Play a game with a continuous 2D function, specifying the method used to
+--   render the field.
+--
 playFieldWith
     :: Arrays world
-    => Render
-    -> Display
-    -> (Int, Int)
-    -> Int
-    -> state
-    -> (state -> world)
+    => Render           -- ^ Method to render the field
+    -> Display          -- ^ Display mode
+    -> (Int, Int)       -- ^ Number of pixels to draw per point
+    -> Int              -- ^ Number of simulation steps to take for each second of real time
+    -> state            -- ^ The initial state
+    -> (state -> world) -- ^ Extract the world state
     -> (Acc world -> Exp Point -> Exp Color)
+            -- ^ Compute the colour of the world at a given point
     -> (Event -> state -> state)
+            -- ^ Handle input events
     -> (Float -> state -> state)
+            -- ^ Step the world one iteration.
+            --   It is passed the time in seconds since the program started.
     -> IO ()
-playFieldWith backend display (zoomX, zoomY) stepRate
+playFieldWith render display (zoomX, zoomY) stepRate
               initState makeWorld makePixel handleEvent stepState
   | zoomX < 1 || zoomY < 1
   = error "Graphics.Gloss.Raster.Field: invalid pixel scalar factor"
@@ -119,7 +131,7 @@ playFieldWith backend display (zoomX, zoomY) stepRate
         -- Size of the raw image to render
         (winSizeX, winSizeY)    = sizeOfDisplay display
 
-        picture                 = makePicture backend winSizeX winSizeY zoomX zoomY makePixel
+        picture                 = makePicture render winSizeX winSizeY zoomX zoomY makePixel
                                 . makeWorld
     in
     play display G.black stepRate initState picture handleEvent stepState
@@ -143,13 +155,13 @@ makePicture
     -> Int                                      -- ^ pixel height
     -> (Acc world -> Exp Point -> Exp Color)    -- ^ function to apply at each point
     -> (world -> Picture)                       -- ^ new function that generates the picture
-makePicture backend winSizeX winSizeY zoomX zoomY makePixel
+makePicture render winSizeX winSizeY zoomX zoomY makePixel
   = let -- Size of the raw image to render
         sizeX           = winSizeX `div` zoomX
         sizeY           = winSizeY `div` zoomY
 
         -- Compute the image
-        pixels          = makeField backend sizeX sizeY makePixel
+        pixels          = makeField render sizeX sizeY makePixel
 
         -- Turn the array into a Gloss picture
         picture world   = bitmapOfArray (pixels world) False
@@ -158,13 +170,13 @@ makePicture backend winSizeX winSizeY zoomX zoomY makePixel
 
 makeField
     :: Arrays world
-    => Render                                   -- ^ method to compute field
+    => Render                                   -- ^ method to compute the field
     -> Int                                      -- ^ width
     -> Int                                      -- ^ height
     -> (Acc world -> Exp Point -> Exp Color)    -- ^ function to apply at each point
     -> (world -> Array DIM2 Word32)             -- ^ new function that computes the image
-makeField backend sizeX sizeY makePixel
-  = run1 backend
+makeField render sizeX sizeY makePixel
+  = render
   $ \world -> A.generate (constant (Z :. sizeY :. sizeX))
                          (packRGBA . opaque . makePixel world . pointOfIndex sizeX sizeY)
 
--- Graphics/Gloss/Accelerate/Render.hs
+++ Graphics/Gloss/Accelerate/Render.hs
@@ -1,53 +1,29 @@
-{-# LANGUAGE CPP #-}
+{-# LANGUAGE CPP        #-}
+{-# LANGUAGE RankNTypes #-}
 
--- | Accelerate backends we know about.
---
 module Graphics.Gloss.Accelerate.Render
   where
 
 import Data.Array.Accelerate                            ( Arrays, Acc )
-import qualified Data.Array.Accelerate.Interpreter      as Interp
+
 #ifdef ACCELERATE_CUDA_BACKEND
-import qualified Data.Array.Accelerate.CUDA             as CUDA
+import Data.Array.Accelerate.CUDA
+#else
+import Data.Array.Accelerate.Interpreter
 #endif
 
 
--- | The set of backends available to execute Accelerate programs. The base
---   functions choose `maxBound' as the default, so there should be some honesty
---   in how this list is sorted.
+-- | The type of executing Accelerate computations. Some variants of the display
+--   functions take an argument of this type, which determine how computations
+--   are executed.
 --
-data Render = Interpreter
-#ifdef ACCELERATE_CUDA_BACKEND
-            | CUDA
-#endif
-  deriving (Eq, Show, Bounded)
+type Render = forall a b. (Arrays a, Arrays b) => (Acc a -> Acc b) -> a -> b
 
 
--- | Execute Accelerate programs
+-- | The set of backends available to execute Accelerate programs. The default
+--   chooses the fastest available, which is currently the CUDA backend. This is
+--   controlled via the import statements.
 --
-run :: Arrays a => Render -> Acc a -> a
-run Interpreter = Interp.run
-#ifdef ACCELERATE_CUDA_BACKEND
-run CUDA        = CUDA.run
-#endif
-
-
--- | Execute an Accelerate program of one argument
---
-run1 :: (Arrays a, Arrays b) => Render -> (Acc a -> Acc b) -> a -> b
-run1 Interpreter f = Interp.run1 f
-#ifdef ACCELERATE_CUDA_BACKEND
-run1 CUDA        f = CUDA.run1 f
-#endif
-
-
--- | The list of available renderers with a short descriptive string
---
-availableRenderers :: [(Render, String)]
-availableRenderers
-  = [ ( Interpreter,    "reference implementation (sequential)" )
-#ifdef ACCELERATE_CUDA_BACKEND
-    , ( CUDA,           "implementation for NVIDIA GPUs (parallel)" )
-#endif
-    ]
+defaultRender :: Render
+defaultRender =  run1
 
